98fd6e8c5f9ed16838471054e3d8689f6cc6fda8
felipedofd <[Dfelipedofd@hotmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C>
2022-08-03 18:40:19 -0300

    create intent to pass data to other activity

--- app/src/main/java/com/example/intentstudies/MainActivity.kt
+++ app/src/main/java/com/example/intentstudies/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.intentstudies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.intentstudies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val messageInEditText = binding.editText.text.toString()
+            val intent = Intent(this, ReceiveIntentActivity::class.java)
+            intent.putExtra("editTextViaIntent", messageInEditText)
+            startActivity(intent)
+        }
     }
 }
